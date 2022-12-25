06fcfdde9d0ea8888a8abb265ba903b3710db8a9
Jeff Kranenburg <jwkranenburg@icloud.com>
2022-12-26 09:22:07 +1300

    setup wp-auto scripts

---
+++ setup-wp-git.sh
@@ -0,0 +1,9 @@
+mkdir site && cd site && \
+git init && \
+curl https://raw.githubusercontent.com/github/gitignore/master/WordPress.gitignore > .gitignore && \
+curl -LO https://wordpress.org/latest.zip && \
+unzip latest.zip && \
+rm latest.zip && \
+mv wordpress/* ./ && \
+git add --all && \
+git commit -sq -m "Initial commit of Wordpress core files. Hello World."
---
+++ setup-wp-plain.sh
@@ -0,0 +1,4 @@
+curl -O https://wordpress.org/latest.zip
+unzip latest.zip 
+mv wordpress site
+rm latest.zip 
