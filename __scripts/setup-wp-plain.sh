51fc1bb31d7ae6640cda03668c12af4fbb143070
Jeff Kranenburg <jwkranenburg@icloud.com>
2022-12-31 21:37:51 +1300

    Update some configs

--- __scripts/setup-wp-plain.sh
+++ __scripts/setup-wp-plain.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkdir -p ../wordpress
+mkdir -p wordpress
 curl -O https://wordpress.org/latest.zip
 unzip latest.zip 
-mv -v wordpress/* ../wordpress/
-cp ../wp-config.php ../wordpress/
-cp ../__dev/dev-wp.env ../wordpress/.env
-rm latest.zip 
+cp wp-config.php wordpress/
+cp __dev/dev-wp.env wordpress/.env
+rm latest.zip
